04a2c86c202748ccba6571ac694a01e340d26da3
moein <moein1475963.mmz@gmail.com>
2021-09-15 20:30:08 +0430

    delete is_holder

--- insured/serializers.py
+++ insured/serializers.py
@@ -11,5 +11,5 @@ class InsuredSerializer(serializers.ModelSerializer):
     class Meta:
         model = Insured
 
-        fields = ['id', 'user', 'insurance', 'is_holder',
+        fields = ['id', 'user', 'insurance',
                   'supported_insureds', 'bank_account_number']
