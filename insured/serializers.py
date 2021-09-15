95d38725d667a97d338cb1323be0eaf56ebd12ce
moein <moein1475963.mmz@gmail.com>
2021-09-15 19:54:09 +0430

    serialize relations of insured

--- insured/serializers.py
+++ insured/serializers.py
@@ -1,20 +1,15 @@
+from authenticate.serializers import UserSerializer
 from rest_framework import serializers
-from . models import Insured
+from .models import Insured
+from authenticate.models import User
 
 
 class InsuredSerializer(serializers.ModelSerializer):
-
-    def get_user_name(self, obj):
-        return obj.user.username
-
-    def get_first_name(self, obj):
-        return obj.user.first_name
-
-    username = serializers.SerializerMethodField("get_user_name")
-    first_name = serializers.SerializerMethodField("get_first_name")
+    user = UserSerializer()
+    supported_insureds = UserSerializer()
 
     class Meta:
         model = Insured
 
-        fields = ['id', 'user','username', 'first_name','insurance', 'is_holder',
+        fields = ['id', 'user', 'insurance', 'is_holder',
                   'supported_insureds', 'bank_account_number']
