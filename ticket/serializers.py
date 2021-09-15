9286ad7b6f7317d29ecf42619a407808ba635109
aminmalek <aminmalek5@gmail.com>
2021-09-16 00:34:00 +0430

    seriaizer added

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,18 +1,10 @@
 from rest_framework import serializers
+from .models import Ticket
 
-class InsuredSerializer(serializers.ModelSerializer):
 
-    def get_user_name(self, obj):
-        return obj.user.username
-
-    def get_first_name(self, obj):
-        return obj.user.first_name
-
-    username = serializers.SerializerMethodField("get_user_name")
-    first_name = serializers.SerializerMethodField("get_first_name")
+class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Insured
-
-        fields = ['id', 'user','username', 'first_name','insurance', 'is_holder',
-                  'supported_insureds', 'bank_account_number']
+        model = Ticket
+        fields = ['name', 'user', 'description', 'is_accepted_by_vendor',
+                  'is_accepted_by_company', 'has_withdrawed']
