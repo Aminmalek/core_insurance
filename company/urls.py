1a9bd158d96c83ce9b54604a9062056646d3b343
moein <moein1475963.mmz@gmail.com>
2021-09-19 02:24:12 +0430

    bugfix user api

--- company/urls.py
+++ company/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
-from .views import CompanyTicketView, VendorActivatedView
 
 urlpatterns = [
-  path('company/tickets', CompanyTicketView.as_view(), name="ticket-company"),
-  path('company/vendor-activation', VendorActivatedView.as_view(),name="vendor-avtivation")
 ]
