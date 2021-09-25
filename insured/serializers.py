42f3eadcd452df90f75d33e319fdd4a07a12230c
moein <moein1475963.mmz@gmail.com>
2021-09-25 11:23:59 +0330

    bugfix insurance serializer

--- insured/serializers.py
+++ insured/serializers.py
@@ -1,5 +1,3 @@
-from payment.serializers import InsuranceConnectorSerializer
-from payment.models import InsuranceConnector
 from authenticate.serializers import UserSerializer
 from rest_framework import serializers
 from .models import Insured
@@ -8,9 +6,7 @@ from .models import Insured
 class InsuredSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     supported_insureds = UserSerializer(many=True)
-    insurance = InsuranceConnectorSerializer(many=True)
 
     class Meta:
         model = Insured
-
         fields = ['id', 'user', 'supported_insureds', 'bank_account_number']
