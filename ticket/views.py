0370ede8dd2ea3b4204d7b55730b9dc12a1f85e1
moein <moein1475963.mmz@gmail.com>
2021-09-26 08:29:18 +0330

    Bugfix ticket put api

--- ticket/views.py
+++ ticket/views.py
@@ -7,7 +7,6 @@ from . serializers import TicketSerializer
 
 
 class TicketView(APIView):
-    
     def get(self, request):
         user = request.user
         if user.type == 'Holder' or user.type == 'Insured':
@@ -26,19 +25,19 @@ class TicketView(APIView):
             description = data['description']
             Ticket.objects.create(
                 user=user, name=ticket_name, description=description)
-            content = {"message": "Ticket created successfuly"}
-            return Response(content, status=status.HTTP_201_CREATED)
+            return Response({"message": "Ticket created successfuly"}, status=status.HTTP_200_OK)
         else:
-            content = {"message": "you are not authenticated to do this"}
-            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
+            return Response({"message": "you are not authorized to perform this action"}, status=status.HTTP_403_FORBIDDEN)
 
     def put(self, request):
         data = request.data
         user = request.user
         if user.type == 'Company':
             ticket_id = data['ticket_id']
+            is_accepted_by_company = data['is_accepted_by_company']
             ticket = Ticket.objects.get(id=ticket_id)
-            ticket.is_accepted_by_company = data['is_accepted_by_company']
+            if is_accepted_by_company:
+                ticket.is_accepted_by_company = True if is_accepted_by_company == 'true' else False
             ticket.save()
             return Response({"message": "Ticket updated successfuly"}, status=status.HTTP_200_OK)
         else:
