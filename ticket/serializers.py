01a1f5539a09d8e30b8a6a55e536cb6e363f4998
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2021-10-01 23:34:33 +0330

    implement claim api

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Ticket
+from .models import Claim, Ticket
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -8,3 +8,9 @@ class TicketSerializer(serializers.ModelSerializer):
         model = Ticket
         fields = ['id', 'name', 'user', 'description', 'is_accepted_by_vendor',
                   'is_accepted_by_company', 'has_withdrawed']
+
+class ClaimSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Claim
+        fields = '__all__'
