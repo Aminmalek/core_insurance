21c640905eda41bea4d878a0681cc03dfe466eaf
aminmalek <aminmalek5@gmail.com>
2021-10-02 11:53:02 +0330

    auth tests final

--- authenticate/tests/test_views.py
+++ authenticate/tests/test_views.py
@@ -120,3 +120,76 @@ class TestGetUserView(APITestCase):
         serializer = UserSerializer(user)
         response = self.client.get(self.url)
         self.assertEqual(serializer.data, response.data)
+
+class TestUserView(APITestCase):
+
+    def setUp(self):
+        self.username = "company1"
+        self.password = "123456"
+        self.type = "Company"
+        self.phone = "3546345"
+        self.user = User.objects.create(
+            username=self.username, password=self.password, type=self.type, phone=self.phone)
+        self.url = reverse("user")
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+    
+    def test_copmany_user_can_view_users(self):
+        User.objects.create(username=687654654654,password=12345678,phone=912456887)
+        User.objects.create(username=96865416354,password=12345678,phone=9198754612)
+        user = User.objects.all()
+        serializer = UserSerializer(user,many=True)
+        response = self.client.get(self.url)
+        self.assertEqual(serializer.data, response.data)
+    
+    def test_none_copmany_user_can_view_users(self):
+        new_user = User.objects.create(
+            username=8751684, 
+            password=123456, 
+            type="Insured", 
+            phone=9104565789
+            )
+       
+        token = Token.objects.create(user=new_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        User.objects.create(username=687654654654,password=12345678,phone=912456887)
+        User.objects.create(username=96865416354,password=12345678,phone=9198754612)
+        user = User.objects.all()
+        serializer = UserSerializer(user,many=True)
+        response = self.client.get(self.url)
+        self.assertEqual(403, response.status_code)
+
+    def test_company_user_can_update_user_data(self):
+        User.objects.create(username=687654654654,password=12345678,phone=912456887,type="Vendor")
+        user = User.objects.get(username=687654654654)
+        data = {
+            "is_active" : True,
+            "type" : "Insured"
+        }
+        types = user.type
+        response = self.client.put("/api/auth/user?id=1",data)
+        self.assertEquals(types,"Vendor")
+
+    def test_none_company_user_can_update_user_data(self):
+        new_user = User.objects.create(
+            username=8751684, 
+            password=123456, 
+            type="Insured", 
+            phone=9104565789
+            )
+       
+        token = Token.objects.create(user=new_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        user = User.objects.create(username=687654654654,password=12345678,phone=912456887,type="Vendor")
+        data = {
+            "is_active" : True,
+            "type" : "Insured"
+        }
+        response = self.client.put("/api/auth/user?id=1",data)
+        
+        self.assertEquals(403, response.status_code,)
