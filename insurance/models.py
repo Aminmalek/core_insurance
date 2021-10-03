74962429195740bf700a8ce3308c282d7a2782c8
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2021-10-03 12:45:21 +0330

    delete unused codes

--- insurance/models.py
+++ insurance/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 
-TYPE_CHOICES = (
-    ('درمانی', 'درمانی'),
-)
-
 
 class Insurance(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     description = models.TextField(max_length=150)
     price = models.IntegerField()
     register_form = models.JSONField()
