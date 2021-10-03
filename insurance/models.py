3c97f060ee8a1378ccdd30bde1681117c9894718
aminmalek <aminmalek5@gmail.com>
2021-10-03 13:19:26 +0330

    insrance tests

--- insurance/models.py
+++ insurance/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
-TYPE_CHOICES = (
-    ('درمانی', 'درمانی'),
-)
-
-
 class Insurance(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     description = models.TextField(max_length=150)
-    price = models.IntegerField()
+    price = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
--- insurance/views.py
+++ insurance/views.py
@@ -21,13 +21,12 @@ class InsuranceView(APIView):
         if user.type == 'Company':
             name = data['name']
             description = data['description']
-            type = data['type']
             price = data['price']
             if Insurance.objects.filter(name=name).exists():
                 return Response({"message": "insurance already exist"}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 Insurance.objects.create(
-                    name=name, description=description, type=type, price=price)
+                    name=name, description=description, price=price)
             return Response({"message": "insurance created successfuly"}, status=status.HTTP_201_CREATED)
         else:
             return Response({"message": "you are not authorized to perform this action"}, status=status.HTTP_403_FORBIDDEN)
@@ -39,12 +38,11 @@ class InsuranceView(APIView):
             data = request.data
             name = data['name']
             description = data['description']
-            type = data['type']
             price = data['price']
             insurance = Insurance.objects.filter(id=id)
             if insurance:
                 insurance.update(
-                    name=name, description=description, type=type, price=price)
+                    name=name, description=description, price=price)
                 return Response({"message": "insurance updated successfuly"}, status=status.HTTP_202_ACCEPTED)
             else:
                 return Response({"message": "insurance doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
