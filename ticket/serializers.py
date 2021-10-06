d65d28178adbb50ff35268676270f9f7dd960795
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2021-10-06 18:27:58 +0330

    user serializer added to ticekt and claim api

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
 from .models import Claim, Ticket
 from payment.serializers import InsuranceConnectorSerializer
+from authenticate.serializers import UserSerializer
+
 
 class TicketSerializer(serializers.ModelSerializer):
-
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Ticket
         fields = '__all__'
 
 class ClaimSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     insurance = InsuranceConnectorSerializer()
     class Meta:
         model = Claim
