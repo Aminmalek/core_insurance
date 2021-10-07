0ffb0e699d84851ae67eaf0ec1fd162186c7eb1e
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2021-10-07 10:48:35 +0330

    add user minified serializer

--- ticket/serializers.py
+++ ticket/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from .models import Claim, Ticket
 from payment.serializers import InsuranceConnectorSerializer
-from authenticate.serializers import UserSerializer
+from authenticate.serializers import UserMinifiedSerializer
 
 
 class TicketSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = UserMinifiedSerializer()
     class Meta:
         model = Ticket
         fields = '__all__'
 
 class ClaimSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = UserMinifiedSerializer()
     insurance = InsuranceConnectorSerializer()
     class Meta:
         model = Claim
