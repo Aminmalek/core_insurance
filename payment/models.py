3e19d76910cbdae013ced51d1858db88a72da3c8
aminmalek <aminmalek5@gmail.com>
2021-10-07 18:53:58 +0330

    insurance connector null = True added

--- payment/models.py
+++ payment/models.py
@@ -10,7 +10,7 @@ class InsuranceConnector(models.Model):
     is_accepted_by_company = models.BooleanField(default=False)
     is_paid = models.BooleanField(default=False)
     payment_code = models.IntegerField(null=True, blank=True)
-    register_form = models.JSONField()
+    register_form = models.JSONField(null=True)
 
     def __str__(self):
         return self.insurance.name
