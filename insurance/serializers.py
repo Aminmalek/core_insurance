618bba47a9311712bf09566f85610c1ff2e8d383
MohammadMahdi MoeinZade <moein1475963.mmz@gmail.com>
2021-10-15 14:51:03 +0330

    update insurance add and update

--- insurance/serializers.py
+++ insurance/serializers.py
@@ -3,6 +3,8 @@ from . models import Insurance
 
 
 class InsuranceSerializer(serializers.ModelSerializer):
+    register_form = serializers.JSONField()
+    claim_form = serializers.JSONField()
     class Meta:
         model = Insurance
         fields = '__all__'
