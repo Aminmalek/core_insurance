2e9d584fe1bb88ec6d7cf5b0e439e59ddff85074
aminmalek <aminmalek5@gmail.com>
2021-10-23 22:46:08 +0330

    buy insurance

--- payment/views.py
+++ payment/views.py
@@ -43,21 +43,8 @@ class InsuranceConnectorView(APIView):
             insurance_id = data['insurance_id']
             register_form = data['register_form']
             insurance = Insurance.objects.get(id=insurance_id)
-            InsuranceConnector.objects.create(
-                user=user, insurance=insurance, register_form=register_form)
-            return Response({"message": "insured created successfuly"}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"message": "you are not authorized to perform this action"}, status=status.HTTP_403_FORBIDDEN)
-
-    def put(self, request, id):
-        data = request.data
-        user = request.user
-        insurance_connector = InsuranceConnector.objects.get(id=id)
-        
-        if user.type == "Holder":
-            user = User.objects.get(username=user.username)
             user_cash = user.cash
-            insurance_price = insurance_connector.insurance.price
+            insurance_price = insurance.price
 
             if int(insurance_price) > int(user_cash):
                 return Response({"message":"you don't have enough money to by insurance"},status=status.HTTP_400_BAD_REQUEST)
@@ -66,14 +53,26 @@ class InsuranceConnectorView(APIView):
                 user.cash = int(user_cash) - int(insurance_price)
                 user.save()
 
-                is_paid = data['is_paid']
-                payment_code = data['payment_code']
-                if is_paid:
-                    insurance_connector.is_paid = True if is_paid == "true" else False
-                if payment_code:
-                    insurance_connector.payment_code = int(payment_code)
-                insurance_connector.save()
-                return Response({"message": "insurance connector updated successfuly"}, status=status.HTTP_200_OK)
+                InsuranceConnector.objects.create(
+                user=user, insurance=insurance, register_form=register_form)
+                return Response({"message": "insured created successfuly"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "you are not authorized to perform this action"}, status=status.HTTP_403_FORBIDDEN)
+
+    def put(self, request, id):
+        data = request.data
+        user = request.user
+        insurance_connector = InsuranceConnector.objects.get(id=id)
+        if user.type == "Holder":
+            user = User.objects.get(username=user.username)
+            is_paid = data['is_paid']
+            payment_code = data['payment_code']
+            if is_paid:
+                insurance_connector.is_paid = True if is_paid == "true" else False
+            if payment_code:
+                insurance_connector.payment_code = int(payment_code)
+            insurance_connector.save()
+            return Response({"message": "insurance connector updated successfuly"}, status=status.HTTP_200_OK)
                 
         elif user.type == "Company":
             is_accepted_by_company = data['is_accepted_by_company']
