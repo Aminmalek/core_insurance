0c3daa6f87d7d25a14a2ab5ef189f269c3f8b2f4
aminmale <aminmalek5@gmail.com>
2021-10-25 23:29:32 +0330

    payment fixed

--- payment/tests/test_views.py
+++ payment/tests/test_views.py
@@ -12,7 +12,7 @@ class PaymentTests(APITestCase):
     def setUp(self):
         """ Tests setup and register user
         """
-        self.username = "Vendor1"
+        self.username = "amin"
         self.password = "123456"
         self.type = "Holder"
         self.cash = 500
@@ -25,37 +25,7 @@ class PaymentTests(APITestCase):
     def api_authentication(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
     
-    def test_5_holder_can_update_insurance_connector(self):
-        insurance=Insurance.objects.create(
-            name = "insu", description = "some thing",price=110)
-        InsuranceConnector.objects.create(id = 874, user = self.user,
-                                          insurance = insurance,
-                                          register_form = {"single": "yes",
-                                                         "sallery": 684,
-                                                         "childes": "No",
-                                                         "sickness": "yes",
-                                                         "sickness name": "AIDS"
-                                                         })
-        data={
-            "insurance_id": insurance.id,
-            "is_accepted_by_company": True,
-            "is_paid": True,
-            "payment_code": 35465,
-            "register_form": {"single": "yes",
-                              "sallery": 684,
-                              "childes": "No",
-                              "sickness": "NO",
-                              "sickness name": "NO"
-                              }
-        }
-
-        response = self.client.put(
-            "/api/insurance_connector/874", format = 'json', data = data)
-        self.assertEqual(200, response.status_code)
-        user = User.objects.get(username=self.username)
-        self.assertEqual(390,user.cash)
-        
-'''
+    
     def test_1_user_vendor_cant_view_insurance_connectors(self):
         user = User.objects.create(
             username="company", password="123456", type="Vendor")
@@ -79,9 +49,9 @@ class PaymentTests(APITestCase):
         self.assertEqual(200, response.status_code)
 
     def test_3_holder_can_post_insurance_connector(self):
-        Insurance.objects.create(id=87,name="insu", description="some thing")
+        Insurance.objects.create(id=847,name="insu", description="some thing",price=25)
         data = {
-            "insurance_id": 87,
+            "insurance_id": 847,
             "is_accepted_by_company": True,
             "is_paid": True,
             "payment_code": 35465,
@@ -95,7 +65,115 @@ class PaymentTests(APITestCase):
         response = self.client.post(self.url, format='json', data=data)
         self.assertEqual(201, response.status_code)
 
-    def test_4_none_holder_user_can_post_insurance_connector(self):
+    def test_4_users_cash_reduced_after_buying_insurance(self):
+        Insurance.objects.create(id=87,name="insu", description="some thing",price=25)
+        data = {
+            "insurance_id": 87,
+            "register_form": {"single": "yes",
+                              "sallery": 684,
+                              "childes": "No",
+                              "sickness": "NO",
+                              "sickness name": "NO"
+                              }
+        }
+        
+        response = self.client.post(self.url, format='json', data=data)
+        user = User.objects.get(username=self.username)
+        connector = InsuranceConnector.objects.get(insurance=87)
+        self.assertEqual(500-25,user.cash)
+
+    def test_5_users_type_changes_after_buy_insurance(self):
+        user = User.objects.create(
+            username="54875466", password="123456", type="Insured",cash=878)
+        self.new_token = Token.objects.create(user=user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.new_token.key)
+        Insurance.objects.create(id=54,name="insu", description="some thing",price=25)
+        data = {
+            "insurance_id": 54,
+            "register_form": {"single": "yes",
+                              "sallery": 684,
+                              "childes": "No",
+                              "sickness": "NO",
+                              "sickness name": "NO"
+                              }
+        }
+        
+        response = self.client.post(self.url, format='json', data=data)
+        user = User.objects.get(username="54875466")
+        connector = InsuranceConnector.objects.get(insurance=54)
+        self.assertEqual("Holder",user.type)
+
+    def test_6_error_if_user_money_is_low(self):
+        user = User.objects.create(
+            username="54875466", password="123456", type="Insured",cash=8)
+        self.new_token = Token.objects.create(user=user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.new_token.key)
+        Insurance.objects.create(id=54,name="insu", description="some thing",price=25)
+        data = {
+            "insurance_id": 54,
+            "register_form": {"single": "yes",
+                            "sallery": 684,
+                            "childes": "No",
+                            "sickness": "NO",
+                            "sickness name": "NO"
+                            }
+        }
+        
+        response = self.client.post(self.url, format='json', data=data)  
+        self.assertEqual(400,response.status_code)
+        self.assertEqual({"error":"you have not enough money to buy insurance"},response.data)
+
+    def test_7_is_paid_changes_after_buy_insurance(self):
+        user = User.objects.create(
+            username="54875466", password="123456", type="Insured",cash=878)
+        self.new_token = Token.objects.create(user=user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.new_token.key)
+        Insurance.objects.create(id=54,name="insu", description="some thing",price=25)
+        data = {
+            "insurance_id": 54,
+            "register_form": {"single": "yes",
+                                "sallery": 684,
+                                "childes": "No",
+                                "sickness": "NO",
+                                "sickness name": "NO"
+                                }
+        }
+        
+        response = self.client.post(self.url, format='json', data=data)
+        user = User.objects.get(username="54875466")
+        connector = InsuranceConnector.objects.get(insurance=54)
+        self.assertEqual(True,connector.is_paid)
+
+    def test_8_company_can_update_insurance_connector(self):
+        user=User.objects.create(
+            username = "company", password = "123456", type = "Company")
+        self.new_token=Token.objects.create(user = user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION = 'Token ' + self.new_token.key)
+        insurance=Insurance.objects.create(
+            id=874,name = "insu", description = "some thing")
+        InsuranceConnector.objects.create(id = 874, user = self.user,
+                                          insurance = insurance,
+                                          register_form = {"single": "yes",
+                                                         "sallery": 684,
+                                                         "childes": "No",
+                                                         "sickness": "yes",
+                                                         "sickness name": "AIDS"
+                                                         },
+                                                         is_accepted_by_company=False)
+        data={
+            "is_accepted_by_company": "true",
+        }
+        response = self.client.put(
+            "/api/insurance_connector/874", format = 'json', data = data)
+        connector = InsuranceConnector.objects.get(insurance=874)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(True, connector.is_accepted_by_company)
+
+    def test_9_none_holder_user_can_post_insurance_connector(self):
         user = User.objects.create(
             username="company", password="123456", type="Company")
         Ticket.objects.create(name="some ticket",
@@ -119,8 +197,7 @@ class PaymentTests(APITestCase):
         response = self.client.post(self.url, format='json', data=data)
         self.assertEqual(403, response.status_code)
 
-
-    def test_6_some_user_can_update_insurance_connector(self):
+    def test_10_some_user_can_update_insurance_connector(self):
         user=User.objects.create(
             username = "company", password = "123456",)
         Ticket.objects.create(name = "some ticket",
@@ -150,35 +227,5 @@ class PaymentTests(APITestCase):
         }
         response = self.client.put(
             "/api/insurance_connector/874", format = 'json', data = data)
-    def test_7_company_can_update_insurance_connector(self):
-        user=User.objects.create(
-            username = "company", password = "123456", type = "Company")
-        Ticket.objects.create(name = "some ticket",
-                              user = user, description = "some thing")
-        self.new_token=Token.objects.create(user = user)
-        self.client.credentials(
-            HTTP_AUTHORIZATION = 'Token ' + self.new_token.key)
-        insurance=Insurance.objects.create(
-            name = "insu", description = "some thing")
-        InsuranceConnector.objects.create(id = 874, user = self.user,
-                                          insurance = insurance,
-                                          register_form = {"single": "yes",
-                                                         "sallery": 684,
-                                                         "childes": "No",
-                                                         "sickness": "yes",
-                                                         "sickness name": "AIDS"
-                                                         })
-        data={
-            "is_accepted_by_company": True,
-            "register_form": {"single": "yes",
-                              "sallery": 684,
-                              "childes": "No",
-                              "sickness": "NO",
-                              "sickness name": "NO"
-                              },
 
-        }
-        response = self.client.put(
-            "/api/insurance_connector/874", format = 'json', data = data)
-        self.assertEqual(200, response.status_code)
-    '''
+   
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query_utils import RegisterLookupMixin
 from authenticate.models import User
 from insured.models import Insured
 from payment.serializers import InsuranceConnectorSerializer
@@ -6,7 +7,7 @@ from payment.models import InsuranceConnector
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from Core.decorators import is_holder_insured
+from Core.decorators import is_company, is_holder_insured
 
 class InsuranceConnectorView(APIView):
     def get(self, request):
@@ -39,42 +40,26 @@ class InsuranceConnectorView(APIView):
         insurance_id = data['insurance_id']
         register_form = data['register_form']
         insurance = Insurance.objects.get(id=insurance_id)
-        InsuranceConnector.objects.create(
-            user=user, insurance=insurance, register_form=register_form)
-        return Response({"message": "insured created successfuly"}, status=status.HTTP_201_CREATED)
+        user = User.objects.get(id=user.id)
+        if int(user.cash) >= int(insurance.price) :
+            user.cash = int(user.cash) - int(insurance.price)
+            InsuranceConnector.objects.create(
+            user=user, insurance=insurance, register_form=register_form, is_paid=True)
+            user.type = "Holder"
+            user.save()
+        else:
+            return Response({"error":"you have not enough money to buy insurance"},status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "insurance purchased successfuly"}, status=status.HTTP_201_CREATED)
 
-    
+    @is_company
     def put(self, request, id):
         data = request.data
-        user = request.user
         insurance_connector = InsuranceConnector.objects.get(id=id)
-        
-        if user.type == "Holder" or user.type == "Insured":
-            user = User.objects.get(id=user.id)
-            insurance_price = insurance_connector.insurance.price
-            user_cash = user.cash
-
-            if int(user_cash) > int(insurance_price) :
-                payment_code = data['payment_code']
-                user.cash = int(user_cash) - int(insurance_price)
-                if user.type == "Insured":
-                    user.type = "Holder"
-                insurance_connector.is_paid = True
-                user.save()
-                insurance_connector.save()
-                #insurance_connector.payment_code = int(payment_code)
-                if payment_code and insurance_connector.is_paid == True:
-                    return Response({"message": "insurance connector updated successfuly"})
-                else:
-                    return Response({"error": "insurance connector updated successfuly"},status.HTTP_406_NOT_ACCEPTABLE)
-            else:
-                return Response({"message":"you don't have enough money to by insurance"},status=status.HTTP_400_BAD_REQUEST)
-
-        elif user.type == "Company":
-            is_accepted_by_company = data['is_accepted_by_company']
-            if is_accepted_by_company:
-                insurance_connector.is_accepted_by_company = True if is_accepted_by_company == "true" else False
-            insurance_connector.save()
-            return Response({"message": "insurance connector updated successfuly"})
+        is_accepted_by_company = data['is_accepted_by_company']
+        if is_accepted_by_company is not None:
+            insurance_connector.is_accepted_by_company = True if is_accepted_by_company == "true" else False
         else:
-            return Response({"message": "you are not authorized to perform this action"}, status=status.HTTP_403_FORBIDDEN)
+            return Response({"error":"please enter correct data"},status=status.HTTP_400_BAD_REQUEST)
+        insurance_connector.save()
+        return Response({"message": "insurance connector updated successfuly"})
+    
