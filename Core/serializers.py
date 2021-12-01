9c0eb302db2233cfb25270dc621cec6b34fc0fa5
aminmalek <aminmalek5@gmail.com>
2021-12-01 23:57:20 +0330

    message serializer chnages

--- Core/serializers.py
+++ Core/serializers.py
@@ -1,9 +1,12 @@
+from authenticate.models import User
 from.models import Message
 from rest_framework import serializers
-
+from authenticate.serializers import UserMinifiedSerializer
 
 class MessageSerializer(serializers.ModelSerializer):
-   
+    sender = UserMinifiedSerializer()
+    receiver = UserMinifiedSerializer()
+
     class Meta:
         model = Message
         fields = '__all__'
